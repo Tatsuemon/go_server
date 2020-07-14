feab241084207599a34120f54625b120561d55f4
Tatsuemon <i10mann-110@ezweb.ne.jp>
2020-07-14 17:13:46 +0900

    hello rest

---
+++ models/users.go
@@ -0,0 +1,42 @@
+package models
+
+import (
+	"fmt"
+	"strconv"
+
+	"github.com/Tatsuemon/go_server/db"
+)
+
+// User defines an user
+type User struct {
+	ID   int    `json:"id"`
+	Name string `json:"name"`
+	Age  int    `json:"age,omitempty"`
+}
+
+// NewUser creates user instance
+func NewUser(name string, age int) *User {
+	user := &User{
+		Name: name,
+		Age:  age,
+	}
+	db.Get().Create(user)
+	fmt.Println(*user)
+	return user
+}
+
+func GetUser(id int) *User {
+	var user *User
+	db.Get().Where("id = ?", id).First(user)
+	return user
+}
+
+func AllUsers() []*User {
+	var users []*User
+	db.Get().Find(&users)
+	return users
+}
+
+func (u *User) String() string {
+	return u.Name + "(" + strconv.Itoa(u.Age) + ")"
+}
--- server.go
+++ server.go
@@ -11,10 +11,21 @@ import (
 func main() {
 	ren := render.New()
 	mux := http.NewServeMux()
-	mux.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
+	mux.HandleFunc("/create", func(w http.ResponseWriter, req *http.Request) {
 		user := models.NewUser("Naoyoshi Aikawa", 29)
 		ren.JSON(w, http.StatusOK, user)
 	})
+
+	mux.HandleFunc("/index", func(w http.ResponseWriter, req *http.Request) {
+		user := models.AllUsers()
+		ren.JSON(w, http.StatusOK, user)
+	})
+
+	mux.HandleFunc("/show", func(w http.ResponseWriter, req *http.Request) {
+		user := models.GetUser(1)
+		ren.JSON(w, http.StatusOK, user)
+	})
+
 	n := negroni.Classic()
 	n.UseHandler(mux)
 	n.Run(":8080")
