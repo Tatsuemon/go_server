6fb0af61185d749d1ad231373bb98a7b484936d5
Tatsuemon <i10mann-110@ezweb.ne.jp>
2020-07-14 14:21:56 +0900

    hello json server

--- memo.md
+++ memo.md
@@ -2,3 +2,5 @@
 - 複数マッチする場合は長い方が優先される
 
 - public/index.htmlを配置すると、それを配信("/")
+
+- json.Marchalは []byte, errorを返すため, stringにキャストする必要あり
--- server.go
+++ server.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
 
 	"github.com/Tatsuemon/go_server/models"
@@ -12,7 +14,14 @@ func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
 		user := models.NewUser("Naoyoshi Aikawa", 29)
-		fmt.Fprintln(w, user)
+		userStr, err := json.Marshal(user)
+		if err != nil {
+			log.Println(err)
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		w.Header().Set("Content-Type", "application/json")
+		fmt.Fprintln(w, string(userStr))
 	})
 	n := negroni.Classic()
 	n.UseHandler(mux)
