ca1266b9445b38b183ad944c6fa8f4df7160ef64
Tatsuemon <i10mann-110@ezweb.ne.jp>
2020-07-14 17:22:50 +0900

    remove first getuser

--- models/users.go
+++ models/users.go
@@ -27,7 +27,7 @@ func NewUser(name string, age int) *User {
 
 func GetUser(id int) *User {
 	var user *User
-	db.Get().Where("id = ?", id).First(user)
+	db.Get().Where("id = ?", id)
 	return user
 }
 
