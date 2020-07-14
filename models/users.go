fc9da1ee7d301689b14431c00dc9472400ce7f06
Tatsuemon <i10mann-110@ezweb.ne.jp>
2020-07-14 17:29:38 +0900

    remove first

--- models/users.go
+++ models/users.go
@@ -26,9 +26,11 @@ func NewUser(name string, age int) *User {
 }
 
 func GetUser(id int) *User {
-	// var user *User
-	return db.Get().Where("id = ?", id)
-	// return user
+	var user *User
+	a := db.Get().Where("id = ?", id)
+	fmt.Println(a)
+	a.First(&user)
+	return user
 }
 
 func AllUsers() []*User {
