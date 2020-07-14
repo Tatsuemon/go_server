1e90c1675eb62b4f5d46fc1849f483c6a6208f62
Tatsuemon <i10mann-110@ezweb.ne.jp>
2020-07-14 14:30:38 +0900

    hello unrolled/render

--- server.go
+++ server.go
@@ -1,27 +1,19 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
-	"log"
 	"net/http"
 
 	"github.com/Tatsuemon/go_server/models"
 	"github.com/codegangsta/negroni"
+	"github.com/unrolled/render"
 )
 
 func main() {
+	ren := render.New()
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
 		user := models.NewUser("Naoyoshi Aikawa", 29)
-		userStr, err := json.Marshal(user)
-		if err != nil {
-			log.Println(err)
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-		w.Header().Set("Content-Type", "application/json")
-		fmt.Fprintln(w, string(userStr))
+		ren.JSON(w, http.StatusOK, user)
 	})
 	n := negroni.Classic()
 	n.UseHandler(mux)
