dee8fe75faf65ec4f9c49791cd56c6f9db782545
Tatsuemon <i10mann-110@ezweb.ne.jp>
2020-07-14 17:03:04 +0900

    hello db with or-mapping

---
+++ db/db.go
@@ -0,0 +1,24 @@
+import (
+	"log"
+
+    _ "github.com/go-sql-driver/mysql"
+    "github.com/jinzhu/gorm"
+    _ "github.com/lib/pq"
+    _ "github.com/mattn/go-sqlite3"
+)
+
+var instance *gorm.DB
+
+// Get gets opened db instance
+func Get() *gorm.DB {
+	if instance != nil {
+		return instance
+	}
+
+	db, err := gorm.Open("postgres", "user=itotatsuhiko dbname=go_server sslmode=disable")
+	if err != nil {
+		log.Println(err)
+	}
+	instance = db
+	return instance
+}
